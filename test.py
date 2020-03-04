e96f98d953c4c8d3279bcd75cbf4b879ce4e10c7
Savio Simoes <41549678+saviosimoes@users.noreply.github.com>
2020-03-04 19:39:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import cv2
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
-img = cv2.imread('savio.jpg')
+img = cv2.imread('imagetobescannedforfaces.jpg')
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
